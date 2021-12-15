45951c1bac621ff59de579bd74aeb8bb30ccdcf9
Shane Yoo <yu.xiang@alibaba-inc.com>
2021-12-15 10:02:06 +0800

    add_scheduler

---
+++ scheduler/src/main/java/scheduler/SchedulerApplication.java
@@ -0,0 +1,15 @@
+package scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
+public class SchedulerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerApplication.class, args);
+    }
+}
