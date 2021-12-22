ba39f253c5e10c139eb18fd8e8d77c631b747825
Shane Yoo <yuxiang0128@gmail.com>
2021-12-22 10:29:15 +0800

    upd_registry

--- registry/src/main/java/online/flyingfish/registry/RegistryApplication.java
+++ registry/src/main/java/online/flyingfish/registry/RegistryApplication.java
@@ -1,4 +1,4 @@
-package flyingfish.online.registry;
+package online.flyingfish.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- registry/src/test/java/online/flyingfish/registry/RegistryApplicationTests.java
+++ registry/src/test/java/online/flyingfish/registry/RegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package flyingfish.online.registry;
+package online.flyingfish.registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
