feb00a0d38cea8fc9aeef16e2e212740d5a348d1
Shane Yoo <yu.xiang@alibaba-inc.com>
2021-12-28 12:03:28 +0800

    upd_office

--- office/src/main/java/online/flyingfish/office/config/WebConfig.java
+++ office/src/main/java/online/flyingfish/office/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
-public class WebConfiguration extends WebMvcConfigurationSupport {
+public class WebConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
